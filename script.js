65e98905f78a22a022cc69cf8f5f2c62bf27927b
izaquedmf <izaquedmf@gmail.com>
2025-02-17 22:00:20 -0300

    make step 2, getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+function getComputerChoice(){
+    const choice = Math.floor(Math.random()*3);
+    switch (choice){
+        case 0:
+            return 'rock';           
+        case 1:
+            return 'paper';          
+        case 2:
+            return 'scissors';
+        default:
+        throw new Error("Invalid choice (omg what happned?)");       
+    }
+}
+
+
